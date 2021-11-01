abb942b69ea292ad4186b4ab01af13e1ccbad08f
Nagendra Chouhan <nikku200109@gmail.com>
2021-11-01 14:47:42 +0530

    update mongoose

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,3 +1,4 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb+srv://vegitable:-LaLG28xPfW$aR_@cluster0.4k2d8.mongodb.net/vegitable").then(
+mongoose.connect("mongodb+srv://vegitable:-LaLG28xPfW$aR_@cluster0.4k2d8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+{useNewUrlParser: true, useUnifiedTopology: true }).then(
     ()=>console.log("connection successfull....")).catch((err)=>console.log(err));
